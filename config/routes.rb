f6e1e7745dd350df143e68bb6f6316464191e07c
sanaljure <sanaljure@gmail.com>
2018-08-08 16:34:34 -0500

    segunda clase ruby on rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get 'welcome/index'
   resources :posts
+  resources :images
   root 'welcome#index'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_185427) do
+ActiveRecord::Schema.define(version: 2018_08_08_185447) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "description"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
